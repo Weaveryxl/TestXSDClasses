a8cd604dc77972c674d6193d5356ffcb4e674263
Weaveryxl <weaveryxl549@gmail.com>
2017-05-12 13:51:55 -0400

    TestXSDClasses

---
+++ TestXSDClasses/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TestXSDClasses
+{
+  [XmlRoot("Customer", Namespace = "http://antra.net")]
+  public class Customer
+  {
+    private string _firstName;
+    private string _middleName;
+    private string _lastName;
+
+    public string LastName
+    {
+	 get { return _lastName; }
+	 set { _lastName = value; }
+    }
+
+    public string FirstName
+    {
+	 get { return _firstName; }
+	 set { _firstName = value; }
+    }
+
+    public string MiddleName
+    {
+	 get { return _middleName; }
+	 set { _middleName = value; }
+    }
+
+    [XmlIgnore]
+
+    public string Name
+    {
+	 get
+	 {
+	   if (string.IsNullOrEmpty(MiddleName) == false)
+		return string.Format("{0} {1} {2}", FirstName, MiddleName, LastName);
+	   else return string.Format("{0} {1}", FirstName, LastName);
+	 }
+    }
+  }
+}
